fd74eeade194795c3975ba5802177aba5f13ea65
Ubuntu <azureuser@MyVm1.bvmv3vpmrxieza14e331ecc0ye.bx.internal.cloudapp.net>
2022-08-19 18:06:28 +0000

    better args

--- iperflaunch.sh
+++ iperflaunch.sh
@@ -1,1 +1,1 @@
-for i in $(seq 1 $1) ; do (iperf3 -s -p 520$i 1>/dev/null &); done 
+for i in $(seq 1 $1) ; do (iperf3 -s -p $((5200+$i)) 1>/dev/null &); done 
--- iperfload.sh
+++ iperfload.sh
@@ -1,1 +1,1 @@
-for i in $(seq 1 $1); do (iperf3 -c 172.19.144.140 -P 4 -t 600 -p 520$i 1>/dev/null &); done 
+for i in $(seq 1 $1); do (iperf3 -c $2 -P 4 -t 600 -p $((5200+$i)) 1>/dev/null &); done 
